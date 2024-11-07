9142ff98a711537051bb16c9e705cab52db6837a
LucasGdBS <lgbs@cesar.school>
2024-11-07 02:04:37 -0300

    movendo a logica dos joins para o main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastcrud import FastCRUD
 from fastcrud import crud_router
 from typing import List
+
+from sqlalchemy import select
 from app.config.db import get_session, lifespan
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.config.db import get_session, lifespan
@@ -9,7 +11,6 @@ from app.models.aluno import Aluno
 from app.schemas.aluno_schema import AlunoCreateSchema
 from app.models.curso import Curso
 from app.models.inscricao import Inscricao
-from app.models.joins import get_alunos_por_curso, get_aluno_cursos
 from app.schemas.curso_schema import CursoCreateSchema
 from app.schemas.inscricao_schema import InscricaoCreateSchema
 from fastapi.middleware.cors import CORSMiddleware
@@ -66,10 +67,17 @@ app.include_router(inscricao_router)
 @app.get("/alunos/curso/{curso_titulo}", response_model=List[AlunoCreateSchema], tags=["Alunos"])
 async def listar_alunos_por_curso(curso_titulo: str, session: AsyncSession = Depends(get_session)):
     try:
-        alunos = await get_alunos_por_curso(curso_titulo, session)
+        query = (
+            select(Aluno)
+            .join(Inscricao, Aluno.id == Inscricao.aluno_id)
+            .join(Curso, Curso.id == Inscricao.curso_id)
+            .where(Curso.titulo == curso_titulo)
+            .distinct()  
+        )
+        alunos = await session.execute(query)
         if not alunos:
             raise HTTPException(status_code=404, detail="Curso não encontrado ou sem alunos")
-        return alunos
+        return alunos.scalars().all()
     except Exception as e:
         print(f"Erro ao listar alunos por curso: {e}")
         raise HTTPException(status_code=500, detail="Erro interno do servidor")
@@ -77,10 +85,15 @@ async def listar_alunos_por_curso(curso_titulo: str, session: AsyncSession = Dep
 @app.get("/alunos/{aluno_id}/cursos", response_model=List[CursoCreateSchema], tags=["Alunos"])
 async def listar_cursos_por_aluno(aluno_id: int, session: AsyncSession = Depends(get_session)):
     try:
-        cursos = await get_aluno_cursos(aluno_id, session)
+        query = (
+            select(Curso)
+            .join(Inscricao, Curso.id == Inscricao.curso_id)
+            .where(Inscricao.aluno_id == aluno_id)
+        )
+        cursos = await session.execute(query)
         if not cursos:
             raise HTTPException(status_code=404, detail="Aluno não encontrado ou sem cursos")
-        return cursos  
+        return cursos.scalars().all()
     except Exception as e:
         print(f"Erro ao listar cursos por aluno: {e}")
         raise HTTPException(status_code=500, detail="Erro interno do servidor")
